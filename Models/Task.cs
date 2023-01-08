1cdaa48d5a34b523a9bf28a884c1fbae30182941
Hubert Jarząbek <hubert.jarzabek@crafton.pl>
2023-01-08 20:24:59 +0100

    initial commit

---
+++ Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication1.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskContent { get; set; }
+        public Task()
+        {
+
+        }
+    }
+}
